d3e962935c8768e1013873b8f86cfdf751bd78fa
Ashwani kumar kushwaha <ashwaniku6266@gmail.com>
2024-05-09 21:39:16 +0530

    update

--- include/file.php
+++ include/file.php
@@ -2,11 +2,11 @@
 <link rel="icon" href="assets/images/logo.png" type="image/x-icon">
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css"> 
 <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" /> 
+<!-- <link rel="stylesheet" href="assets/fontawesome/css/all.min.css"> -->
+<link rel="stylesheet" href="assets/fontawesome/css/fontawesome.min.css">
 <link rel="stylesheet" href="assets/css/elegant-icons.css" type="text/css" />
 <link rel="stylesheet" href="assets/css/magnific-popup.css" type="text/css" />
 <link rel="stylesheet" href="assets/css/nice-select.css" type="text/css" />
 <link rel="stylesheet" href="assets/css/owl.carousel.min.css" type="text/css" />
 <link rel="stylesheet" href="assets/css/slicknav.min.css" type="text/css" />
 <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
-<link rel="stylesheet" href="assets/fontawesome/css/all.min.css">
-<link rel="stylesheet" href="assets/fontawesome/css/fontawesome.min.css">
--- index.php
+++ index.php
@@ -69,14 +69,66 @@
                     <div class="socail_media__under text-center">
                         <ul class=" d-flex justify-content-center align-items-center">
                             <li class="px-4 ">
-                                <a href=""><i class="fa fa-facebook"></i></a>
+                                <a href="">
+                                    <!-- <i class="fa fa-facebook"></i> -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20px">
+                                        <path fill="#fff" d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z" />
+                                    </svg>
+                                </a>
                             </li>
-                            <li class="px-4"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li class="px-4"><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                            <li class="px-4"><a href="#"><i class="fa fa-instagram"></i></a></li>
+                            <li class="px-4"><a href="#">
+                                    <!-- <i class="fa fa-twitter"></i> -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20px">
+                                        <path fill="#fff" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+                                    </svg>
+                                </a>
+                            </li>
+                            <!-- <li class="px-4"><a href="#">
+                                    <i class="fa fa-pinterest"></i>
+                                </a>
+                            </li> -->
+                            <li class="px-4">
+                                <a href="#">
+                                    <!-- <i class="fa fa-instagram"></i> -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="20px">
+                                        <path fill="#fff" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
+                                    </svg>
+                                </a>
+                            </li>
                         </ul>
                     </div>
 
+                </div>
+            </div>
+        </section>
+
+        <section class="banner spad">
+            <div class="container">
+                <div class="row">
+                    
+                    <div class="col-lg-5 ">
+                        <div class="banner__item">
+                            <div class="banner__item__pic">
+                                <img src="assets/images/5.png" alt="Shop now">
+                            </div>
+                            <div class="banner__item__text">
+                                <h2>Clothing Collections 2030</h2>
+                                <a href="#">Shop now</a>
+                            </div>
+                        </div>
+                    </div>
+<div class="col-1"></div>
+                    <div class="col-lg-6">
+                        <div class="banner__item banner__item--last">
+                            <div class="banner__item__pic">
+                                <img src="assets/images/4.png" alt="Shop now">
+                            </div>
+                            <div class="banner__item__text">
+                                <h2>Shoes Spring 2030</h2>
+                                <a href="#">Shop now</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
@@ -92,6 +144,7 @@
     <script src="assets/js/mixitup.min.js"></script>
     <script src="assets/js/owl.carousel.min.js"></script>
     <script src="assets/js/main.js"></script>
+
 
 </body>
 
